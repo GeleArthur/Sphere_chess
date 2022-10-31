86fb338e9e31647ce558c46e9db9f30a2d4742c6
Arthur <geleminecrafter@gmail.com>
2022-10-31 21:14:15 +0100

    Adding screen mouse

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
     input::mouse::MouseMotion,
+    math::*,
     pbr::wireframe::{Wireframe, WireframePlugin},
     prelude::*,
 };
@@ -12,6 +13,7 @@ fn main() {
         .add_plugin(WireframePlugin)
         .add_startup_system(setup_scene)
         .add_system(camera_rotation)
+        .add_system(camera_sphere_select)
         .run();
 }
 
@@ -92,4 +94,42 @@ fn camera_rotation(
         let forward = camera.forward();
         camera.translation = -forward * 5.0;
     }
+}
+
+fn camera_sphere_select(query: Query<(&Camera, &GlobalTransform)>, windows: Res<Windows>) {
+
+    let window = windows.get_primary().unwrap();
+
+    if let Some(_position) = window.cursor_position() {
+        let camera_pos = query.single();
+
+        let pos = from_screenspace(_position, camera_pos.0, camera_pos.1).unwrap();
+
+        print!("{} {} {} \n", pos.0.x, pos.0.y, pos.0.z);
+    }
+}
+
+pub fn from_screenspace(
+    cursor_pos_screen: Vec2,
+    camera: &Camera,
+    camera_transform: &GlobalTransform,
+) -> Option<(Vec3, Vec3)> {
+    let view = camera_transform.compute_matrix();
+
+    let (viewport_min, viewport_max) = camera.logical_viewport_rect()?;
+    let screen_size = camera.logical_target_size()?;
+    let viewport_size = viewport_max - viewport_min;
+    let adj_cursor_pos =
+        cursor_pos_screen - Vec2::new(viewport_min.x, screen_size.y - viewport_max.y);
+
+    let projection = camera.projection_matrix();
+    let far_ndc = projection.project_point3(Vec3::NEG_Z).z;
+    let near_ndc = projection.project_point3(Vec3::Z).z;
+    let cursor_ndc = (adj_cursor_pos / viewport_size) * 2.0 - Vec2::ONE;
+    let ndc_to_world: Mat4 = view * projection.inverse();
+    let near = ndc_to_world.project_point3(cursor_ndc.extend(near_ndc));
+    let far = ndc_to_world.project_point3(cursor_ndc.extend(far_ndc));
+    let ray_direction = far - near;
+
+    return Some((near, ray_direction));
 }
