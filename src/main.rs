4d9ed078bc85d5aa720c729f9aefae3104ff0134
Arthur <geleminecrafter+git@gmail.com>
2022-11-02 08:19:54 +0100

    sdas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 use bevy::{
     input::mouse::MouseMotion,
     math::*,
@@ -5,6 +7,24 @@ use bevy::{
     prelude::*,
 };
 use bevy_inspector_egui::WorldInspectorPlugin;
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+struct CameraRotation {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+struct GizmosCube {}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+struct CenterSphere {
+    center: Vec3,
+    radius: f32,
+}
 
 fn main() {
     App::new()
@@ -30,16 +50,25 @@ fn setup_scene(
         .insert(Name::new("Camera"))
         .insert(CameraRotation { x: 0.0, y: 0.0 });
 
+    let radius = 1.0;
+
     commands
-        .spawn_bundle(PbrBundle {
+        .spawn()
+        .insert(CenterSphere {
+            center: Vec3::new(0.0, 0.0, 0.0),
+            radius: radius,
+        })
+        .insert_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: radius,
                 ..Default::default()
             })),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
             transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..Default::default()
         })
-        .insert(Wireframe);
+        .insert(Wireframe)
+        .insert(Name::new("Sphere"));
 
     commands
         .spawn_bundle(PbrBundle {
@@ -59,13 +88,18 @@ fn setup_scene(
         transform: Transform::from_xyz(0.0, 2.0, 1.0),
         ..Default::default()
     });
-}
 
-#[derive(Reflect, Component, Default)]
-#[reflect(Component)]
-struct CameraRotation {
-    x: f32,
-    y: f32,
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: 0.1,
+                ..Default::default()
+            })),
+            material: materials.add(Color::rgb(1.0, 0.0, 0.1).into()),
+            ..Default::default()
+        })
+        .insert(GizmosCube {})
+        .insert(Name::new("Gizmos"));
 }
 
 fn camera_rotation(
@@ -96,16 +130,72 @@ fn camera_rotation(
     }
 }
 
-fn camera_sphere_select(query: Query<(&Camera, &GlobalTransform)>, windows: Res<Windows>) {
-
+fn camera_sphere_select(
+    camera: Query<(&Camera, &GlobalTransform)>,
+    mut gizmos_cube: Query<&mut Transform, With<GizmosCube>>,
+    sphere: Query<&CenterSphere>,
+    windows: Res<Windows>,
+) {
     let window = windows.get_primary().unwrap();
 
     if let Some(_position) = window.cursor_position() {
-        let camera_pos = query.single();
+        let camera_pos = camera.single();
 
-        let pos = from_screenspace(_position, camera_pos.0, camera_pos.1).unwrap();
+        let raycasted = from_screenspace(_position, camera_pos.0, camera_pos.1).unwrap();
+        let mut giz_transform = gizmos_cube.single_mut();
 
-        print!("{} {} {} \n", pos.0.x, pos.0.y, pos.0.z);
+        let diraction = Vec3::new(raycasted.0.x, raycasted.0.y, raycasted.0.z)
+            + Vec3::new(raycasted.1.x, raycasted.1.y, raycasted.1.z).mul(1.0);
+
+        let sphere = sphere.single();
+
+        let to_sphere = raycasted.0 - sphere.center;
+        let distance_to_sphere = to_sphere.length();
+
+        if sphere.radius * 2.0 > distance_to_sphere {
+            print!("We are inside {} {}\n", distance_to_sphere, sphere.radius);
+            return;
+        }
+
+        let result = to_sphere.normalize().dot(raycasted.1.normalize());
+
+        if result <= 0.0 {
+            let v = to_sphere - Vec3::project_onto(to_sphere, raycasted.1);
+            let v_squerd = v.dot(v);
+            let r_squard = sphere.radius * sphere.radius;
+
+            if v_squerd <= r_squard {
+                giz_transform.translation = sphere.center + v - (raycasted.1.normalize() * (f32::sqrt(r_squard-v_squerd)))
+            }
+        } else {
+            print!("No collison")
+        }
+
+        // giz_transform.translation = diraction;
+
+        // let A = Vec3::new(pos.0.x, pos.0.y, pos.0.z);
+        // let d = Vec3::new(pos.1.x, pos.1.y, pos.1.z);
+
+        // let C = Vec3::ZERO;
+        // let r = 5.0;
+        // let rSquared = r*r;
+
+        // let CA = A-C;
+
+        // let v = CA - Vec3::project_onto(CA, d);
+        // let vSquared = Vec3::dot(v,v);
+
+        // let collisionPoint;
+
+        // if vSquared <= rSquared {
+        //     collisionPoint = C + v - (d.normalize() * f32::sqrt(rSquared-vSquared));
+        // }else{
+        //     collisionPoint = Vec3::ZERO;
+        // }
+
+        // giz_transform.translation = collisionPoint;
+
+        // print!("{} {} {} \n", collisionPoint.x, collisionPoint.y, collisionPoint.z);
     }
 }
 
